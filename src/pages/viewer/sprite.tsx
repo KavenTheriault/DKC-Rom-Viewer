586219c158466522b76ac956e0f65a6f91c6c341
Kaven Theriault <theriault.kaven@gmail.com>
2024-02-24 11:07:04 -0500

    Add sprite parts

--- src/pages/viewer/sprite.tsx
+++ src/pages/viewer/sprite.tsx
@@ -1,16 +1,21 @@
 import { ChangeEvent, useState } from 'react';
 import { SelectedRom } from '../../types/selected-rom';
-import { getSpriteHeader, SpriteHeader } from '../../rom-parser/sprites/header';
 import { RomAddress } from '../../rom-parser/types/address';
 import { SpriteHeaderTable } from './components/sprite-header';
 import { validateSpriteHeader } from '../../rom-parser/scan/sprites';
 import {
   getAddressFromSpritePointerIndex,
   readSprite,
+  Sprite,
 } from '../../rom-parser/sprites';
 import { toHexString } from '../../utils/hex';
 import { ImageCanvas } from '../../components/image-canvas';
-import { Array2D, Color, Image } from '../../rom-parser/sprites/types';
+import {
+  Array2D,
+  Color,
+  Image,
+  SpritePart,
+} from '../../rom-parser/sprites/types';
 import {
   buildImageFromPixelsAndPalette,
   readPalette,
@@ -22,12 +27,24 @@ interface SpriteViewerProps {
 }
 
 const isHexadecimal = (str: string) => /^[0-9A-F]+$/.test(str);
+const spritePartString = (spritePart: SpritePart) => {
+  const address =
+    spritePart.type === '8x8'
+      ? spritePart.tile.address
+      : spritePart.tile.tiles[0].address;
+
+  return `${toHexString(address.snesAddress, { addPrefix: true })} - (${spritePart.type})`;
+};
+const spritePartTiles = (spritePart: SpritePart) => {
+  return spritePart.type === '8x8' ? [spritePart.tile] : spritePart.tile.tiles;
+};
 
 export const SpriteViewer = ({ selectedRom }: SpriteViewerProps) => {
   const [snesAddress, setSnesAddress] = useState<string>('');
   const [spritePointer, setSpritePointer] = useState<string>('');
-  const [spriteHeader, setSpriteHeader] = useState<SpriteHeader>();
+  const [sprite, setSprite] = useState<Sprite>();
   const [spriteImage, setSpriteImage] = useState<Image>();
+  const [selectedSpritePart, setSelectedSpritePart] = useState<SpritePart>();
   const [error, setError] = useState('');
 
   const onSnesAddressChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -50,7 +67,7 @@ export const SpriteViewer = ({ selectedRom }: SpriteViewerProps) => {
       loadSprite(RomAddress.fromSnesAddress(parsedSnesAddress));
       setSpritePointer('');
     } else {
-      setSpriteHeader(undefined);
+      setSprite(undefined);
     }
   };
 
@@ -60,16 +77,8 @@ export const SpriteViewer = ({ selectedRom }: SpriteViewerProps) => {
       loadSpritePointer(parsedSpritePointer);
     } else {
       setSnesAddress('');
-      setSpriteHeader(undefined);
+      setSprite(undefined);
     }
-  };
-
-  const onNextSpritePointerClick = () => {
-    offsetSpritePointer(4);
-  };
-
-  const onPreviousSpritePointerClick = () => {
-    offsetSpritePointer(-4);
   };
 
   const offsetSpritePointer = (offset: number) => {
@@ -93,27 +102,26 @@ export const SpriteViewer = ({ selectedRom }: SpriteViewerProps) => {
   };
 
   const loadSprite = (spriteAddress: RomAddress) => {
-    const header = getSpriteHeader(selectedRom.data, spriteAddress);
-    setSpriteHeader(header);
+    const loadedSprite = readSprite(selectedRom.data, spriteAddress);
+    setSprite(loadedSprite);
 
-    if (header && validateSpriteHeader(header)) {
+    if (loadedSprite && validateSpriteHeader(loadedSprite.header)) {
       setError('');
-      buildSpriteImage(spriteAddress);
+      setSelectedSpritePart(loadedSprite.parts[0]);
+      buildSpriteImage(loadedSprite);
     } else {
       setError('Invalid Sprite Header');
       setSpriteImage(undefined);
+      setSelectedSpritePart(undefined);
     }
   };
 
-  const buildSpriteImage = (spriteAddress: RomAddress) => {
-    const sprite = readSprite(selectedRom.data, spriteAddress);
-    if (!sprite) return;
-
+  const buildSpriteImage = (spriteToBuild: Sprite) => {
     const palette: Color[] = readPalette(
       selectedRom.data,
       RomAddress.fromSnesAddress(0xbc849a),
     );
-    const spritePixels: Array2D = assembleSprite(sprite.parts);
+    const spritePixels: Array2D = assembleSprite(spriteToBuild.parts);
     const image: Image = buildImageFromPixelsAndPalette(spritePixels, palette);
     setSpriteImage(image);
   };
@@ -121,7 +129,7 @@ export const SpriteViewer = ({ selectedRom }: SpriteViewerProps) => {
   return (
     <div>
       <div className="columns">
-        <div className="column is-one-third">
+        <div className="column">
           <div className="block">
             <label className="label">SNES Address</label>
             <div className="field has-addons">
@@ -173,7 +181,9 @@ export const SpriteViewer = ({ selectedRom }: SpriteViewerProps) => {
               <p className="control">
                 <a
                   className="button is-primary is-outlined"
-                  onClick={onPreviousSpritePointerClick}
+                  onClick={() => {
+                    offsetSpritePointer(-4);
+                  }}
                 >
                   -4
                 </a>
@@ -181,37 +191,101 @@ export const SpriteViewer = ({ selectedRom }: SpriteViewerProps) => {
               <p className="control">
                 <a
                   className="button is-primary is-outlined"
-                  onClick={onNextSpritePointerClick}
+                  onClick={() => {
+                    offsetSpritePointer(4);
+                  }}
                 >
                   +4
                 </a>
               </p>
             </div>
           </div>
+
+          {error && (
+            <div className="notification is-warning">
+              <button className="delete" onClick={() => setError('')}></button>
+              {error}
+            </div>
+          )}
+
+          {sprite && (
+            <nav className="panel is-info">
+              <p className="panel-heading">Sprite Header</p>
+              <div className="panel-block">
+                <SpriteHeaderTable spriteHeader={sprite.header} />
+              </div>
+            </nav>
+          )}
         </div>
         <div className="column">
           {spriteImage && <ImageCanvas image={spriteImage} />}
         </div>
-      </div>
-
-      {error && (
-        <div className="notification is-warning">
-          <button className="delete" onClick={() => setError('')}></button>
-          {error}
-        </div>
-      )}
-
-      <div className="columns">
-        {spriteHeader && (
-          <div className="column is-one-third">
-            <nav className="panel is-info">
-              <p className="panel-heading">Sprite Header</p>
-              <div className="panel-block">
-                <SpriteHeaderTable spriteHeader={spriteHeader} />
+        <div className="column">
+          <label className="label">Sprite Parts</label>
+          <div className="columns">
+            <div className="column">
+              <div className="select is-multiple">
+                <select
+                  multiple
+                  size={10}
+                  onChange={(e) =>
+                    setSelectedSpritePart(
+                      sprite?.parts[parseInt(e.target.value)],
+                    )
+                  }
+                >
+                  {sprite &&
+                    sprite.parts.map((spritePart, index) => (
+                      <option value={index}>
+                        {spritePartString(spritePart)}
+                      </option>
+                    ))}
+                </select>
               </div>
-            </nav>
+            </div>
+            {selectedSpritePart && (
+              <div className="column">
+                <div className="field">
+                  <label className="label">X</label>
+                  <div className="control">
+                    <input
+                      className="input"
+                      type="number"
+                      value={selectedSpritePart.coordinate.x}
+                      readOnly
+                    />
+                  </div>
+                </div>
+                <div className="field">
+                  <label className="label">Y</label>
+                  <div className="control">
+                    <input
+                      className="input"
+                      type="number"
+                      value={selectedSpritePart.coordinate.y}
+                      readOnly
+                    />
+                  </div>
+                </div>
+                {spritePartTiles(selectedSpritePart).map((tile, index) => (
+                  <div className="field">
+                    <label className="label">Tile {index + 1}</label>
+                    <div className="control">
+                      <input
+                        className="input"
+                        type="text"
+                        value={toHexString(tile.address.snesAddress, {
+                          addPrefix: true,
+                        })}
+                        readOnly
+                      />
+                    </div>
+                  </div>
+                ))}
+              </div>
+            )}
           </div>
-        )}
+        </div>
       </div>
     </div>
   );
