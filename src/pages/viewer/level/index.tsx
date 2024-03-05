f07d86cf35e6687a653d95ec0e5a6a3cdbc03cb2
Kaven Theriault <theriault.kaven@gmail.com>
2024-03-04 19:16:55 -0500

    Bitplane image working

--- src/pages/viewer/level/index.tsx
+++ src/pages/viewer/level/index.tsx
@@ -3,6 +3,7 @@ import { extract } from '../../../rom-parser/utils/buffer';
 import { ViewerModeBaseProps } from '../types';
 import { RomAddress } from '../../../rom-parser/types/address';
 import { ImageCanvas } from '../../../components/image-canvas';
+import { Image } from '../../../rom-parser/sprites/types';
 
 export const LevelViewer = ({ selectedRom }: ViewerModeBaseProps) => {
   const test = RomAddress.fromSnesAddress(0x231180);
@@ -10,42 +11,14 @@ export const LevelViewer = ({ selectedRom }: ViewerModeBaseProps) => {
   const decompressedBitplane = decompressDKC1(data);
   const bitplaneImage = dumpBitplane(decompressedBitplane);
 
-  /* const tilesData = chunk(test2, TILE_DATA_LENGTH);
-  const tiles = tilesData.map((td) => parsePixels(Buffer.from(td)));
-
-  // Test code
-  const rows = chunk(tiles, 16);
-
-  const totalTilesToShow = tiles.length - (tiles.length % 16);
-
-  const width = 16;
-  const height = totalTilesToShow / 16;
-
-  const allPixels: Array2D = create2DArray(width * 8, height * 8);
-
-  for (let c = 0; c < width; c++) {
-    for (let r = 0; r < height; r++) {
-      const tile = rows[r][c];
-
-      for (let x = 0; x < tile.length; x++) {
-        for (let y = 0; y < tile[0].length; y++) {
-          allPixels[x + c * 8][y + r * 8] = tile[x][y];
-        }
-      }
-    }
-  }
-
-  const palette = readPalette(
-    selectedRom.data,
-    RomAddress.fromSnesAddress(0x39a1dc),
-  );
-  const image: Image = buildImageFromPixelsAndPalette(allPixels, palette);*/
-
   return (
     <div className="columns">
       <pre>{bitplaneImage.length}</pre>
       <div className="column">
-        <ImageCanvas defaultSize={{ width: 256, height: 256 }} />
+        <ImageCanvas
+          image={bitplaneImage}
+          defaultSize={{ width: 256, height: 256 }}
+        />
       </div>
     </div>
   );
@@ -93,59 +66,58 @@ const decompressDKC1 = (compressed: Buffer) => {
   return decompressed;
 };
 
-const dumpBitplane = (bitplane: number[]) => {
-  const width = 16;
-  const tileCount = bitplane.length / 32;
+const TILES_PER_ROW = 16;
+const TILE_PIXEL_WIDTH = 8;
+const TILE_PIXEL_HEIGHT = 8;
+const BYTES_PER_TILE = 32;
+
+const createEmptyImage = (width: number, height: number): Image =>
+  new Array(width).fill(0).map(() => new Array(height).fill(null));
+
+const dumpBitplane = (bitplaneData: number[]) => {
   const bitsPerPixel = 4;
 
-  let pxHeight = ((tileCount - (tileCount % width)) / width) * 8;
-  if (tileCount % width) pxHeight += 8;
-  const pxWidth = width * 8;
+  const tileCount = bitplaneData.length / BYTES_PER_TILE;
+  const totalTileRows = Math.ceil(tileCount / TILES_PER_ROW);
 
-  const imgSize = pxWidth * pxHeight * 4;
-  const image: number[] = new Array(imgSize).fill(0);
-
-  let offset = 0;
+  const pxHeight = totalTileRows * TILE_PIXEL_HEIGHT;
+  const pxWidth = TILES_PER_ROW * TILE_PIXEL_WIDTH;
+  const image: Image = createEmptyImage(pxWidth, pxHeight);
 
   const bits: number[] = [128, 64, 32, 16, 8, 4, 2, 1];
   const byte = new Array(4).fill(0);
 
-  for (let i = 0; i < tileCount; i++) {
-    // Each Tile
-    const tile_n = i % width;
-    if (i > width - 1) {
-      offset = ((i - (i % width)) / width) * (pxWidth * 8 * 4);
-    }
+  for (let tileIndex = 0; tileIndex < tileCount; tileIndex++) {
+    const currentTileRow =
+      (tileIndex - (tileIndex % TILES_PER_ROW)) / TILES_PER_ROW;
 
-    for (let j = 0; j < 8; j++) {
+    const widthPixelOffset = (tileIndex % TILES_PER_ROW) * TILE_PIXEL_WIDTH;
+    const heightPixelOffset = currentTileRow * TILE_PIXEL_HEIGHT;
+
+    for (let j = 0; j < TILE_PIXEL_HEIGHT; j++) {
       // Each line
-      byte[0] = bitplane[i * (bitsPerPixel * 8) + j * 2];
-      byte[1] = bitplane[i * (bitsPerPixel * 8) + j * 2 + 1];
-      byte[2] = bitplane[i * (bitsPerPixel * 8) + j * 2 + 16];
-      byte[3] = bitplane[i * (bitsPerPixel * 8) + j * 2 + 17];
+      byte[0] = bitplaneData[tileIndex * (bitsPerPixel * 8) + j * 2];
+      byte[1] = bitplaneData[tileIndex * (bitsPerPixel * 8) + j * 2 + 1];
+      byte[2] = bitplaneData[tileIndex * (bitsPerPixel * 8) + j * 2 + 16];
+      byte[3] = bitplaneData[tileIndex * (bitsPerPixel * 8) + j * 2 + 17];
 
-      for (let k = 0; k < 8; k++) {
+      for (let k = 0; k < TILE_PIXEL_WIDTH; k++) {
         // Each plane
         let value = 0;
 
-        if ((byte[0] & bits[k]) == bits[k]) value += 1;
-        if ((byte[1] & bits[k]) == bits[k]) value += 2;
-        if ((byte[2] & bits[k]) == bits[k]) value += 4;
-        if ((byte[3] & bits[k]) == bits[k]) value += 8;
+        if ((byte[0] & bits[k]) === bits[k]) value += 1;
+        if ((byte[1] & bits[k]) === bits[k]) value += 2;
+        if ((byte[2] & bits[k]) === bits[k]) value += 4;
+        if ((byte[3] & bits[k]) === bits[k]) value += 8;
 
         // Bits Per Pixel of 4
         value *= 16;
 
-        const pixelIndex = offset + j * pxWidth * 4 + tile_n * 32 + k * 4;
+        const pixelX = widthPixelOffset + k;
+        const pixelY = heightPixelOffset + j;
 
-        image[pixelIndex] = value;
-        image[pixelIndex + 1] = value;
-        image[pixelIndex + 2] = value;
-
-        if (value == 0) {
-          image[pixelIndex + 3] = 0;
-        } else {
-          image[pixelIndex + 3] = 255;
+        if (value !== 0) {
+          image[pixelX][pixelY] = { r: value, g: value, b: value };
         }
       }
     }
