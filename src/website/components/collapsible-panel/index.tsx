c8c667d7e5e5b9ed45171acd8645a862f8a925fa
Kaven Thériault <theriault.kaven@gmail.com>
2026-01-01 10:31:08 -0500

    Keep collapsed panel in DOM

--- src/website/components/collapsible-panel/index.tsx
+++ src/website/components/collapsible-panel/index.tsx
@@ -12,34 +12,40 @@ export const CollapsiblePanel = ({
 }: CollapsiblePanelProps) => {
   const [isOpen, setIsOpen] = React.useState(true);
 
-  const renderShowHideButton = () => {
-    return (
-      <button
-        className="button is-small is-outlined is-rounded"
-        onClick={() => setIsOpen(!isOpen)}
-      >
-        <span className="icon is-small">
-          <i
-            className={isOpen ? 'fas fa-eye-slash' : 'fas fa-eye'}
-            aria-hidden="true"
-          ></i>
-        </span>
-        {!isOpen && <span>{title}</span>}
-      </button>
-    );
-  };
+  const renderShowHideButton = ({ isHidden }: { isHidden: boolean }) => (
+    <button
+      className={[
+        'button',
+        'is-small',
+        'is-outlined',
+        'is-rounded',
+        ...(isHidden ? ['is-hidden'] : []),
+      ].join(' ')}
+      onClick={() => setIsOpen(!isOpen)}
+    >
+      <span className="icon is-small">
+        <i
+          className={isOpen ? 'fas fa-eye-slash' : 'fas fa-eye'}
+          aria-hidden="true"
+        ></i>
+      </span>
+      {!isOpen && <span>{title}</span>}
+    </button>
+  );
 
-  if (!isOpen) return renderShowHideButton();
   return (
-    <Box className="box">
-      <div className="is-flex is-flex-direction-column">
-        <div className="pt-1 is-flex is-flex-direction-row is-align-items-center">
-          {renderShowHideButton()}
-          <span className="pl-2 is-size-6 has-text-weight-bold">{title}</span>
+    <>
+      {renderShowHideButton({ isHidden: isOpen })}
+      <Box className={['box', ...(!isOpen ? ['is-hidden'] : [])].join(' ')}>
+        <div className="is-flex is-flex-direction-column">
+          <div className="pt-1 is-flex is-flex-direction-row is-align-items-center">
+            {renderShowHideButton({ isHidden: !isOpen })}
+            <span className="pl-2 is-size-6 has-text-weight-bold">{title}</span>
+          </div>
+          <HeaderHr className="has-background-grey-lighter" />
         </div>
-        <HeaderHr className="has-background-grey-lighter" />
-      </div>
-      {children}
-    </Box>
+        {children}
+      </Box>
+    </>
   );
 };
