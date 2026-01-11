7ee5cefb305efe08e733cfa4fa0c970494ab208a
Kaven Thériault <theriault.kaven@gmail.com>
2026-01-11 09:26:05 -0500

    Fix visible scrollbar in Chrome

--- src/website/index.tsx
+++ src/website/index.tsx
@@ -2,6 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Router } from './router';
 import { AppStateProvider } from './state/state-provider';
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  html {
+    /* Override Bulma’s forced vertical scrollbar */
+    overflow-y: auto;
+  }
+`;
 
 export const startApp = () => {
   const root = ReactDOM.createRoot(
@@ -9,6 +17,7 @@ export const startApp = () => {
   );
   root.render(
     <React.StrictMode>
+      <GlobalStyle />
       <AppStateProvider>
         <Router />
       </AppStateProvider>
